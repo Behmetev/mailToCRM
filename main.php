57053a3ef39c4042c4289b24668c329b916854fb
Nikita Behmetev <nikitabehmetev@gmail.com>
2023-12-18 12:19:12 +0300

    Отправка данных в Б24

--- main.php
+++ main.php
@@ -48,24 +48,69 @@ foreach ($messages as $message) {
 		$textMail =  $message->getHTMLBody() . '\n';
 		$flags = $message->getFlags()/*->collect('seen')*/;
 		$textMail = explode("<br>", $textMail);
-		//print_r($flags["seen"]);
+
+		if (!isset($flags["seen"])) {
+			$queryURL = $prop['b24Url'] . "/rest/" . $prop['b24UserID'] . "/" . $prop['b24WebHook'] . "/crm.lead.add.json";
 
 
-		if (isset($flags["seen"])) {
-			echo "isset";
+			if (str_contains($textMail[1], 'Дата отправки')) {
+				unset($textMail[1]);
+			}
+			if (str_contains($textMail[2], 'Время лида на момент заполнения формы')) {
+				unset($textMail[2]);
+			}
+
+			$textMail = array_values($textMail);
+			$mailTitle = explode(": ", $textMail[0]);
+			$mailName = explode(": ", $textMail[2]);
+			$mailTelephone = explode(": ", $textMail[3]);
+			$mailCity = explode(": ", $textMail[4]);
+
+			echo $mailTitle[1] . "\n";
+			echo $mailName[1] . "\n";
+			echo $mailCity[1] . "\n";
+			echo $mailTelephone[1] . "\n";
+
+			// формируем параметры для создания лида
+			$queryData = http_build_query(array(
+				"fields" => array(
+					"TITLE" => $mailTitle[1],
+					"NAME" => $mailName[1],
+					"ADDRESS_CITY" => $mailCity[1],
+					"PHONE" => array(
+						"n0" => array(
+							"VALUE" =>  $mailTelephone[1],
+							"VALUE_TYPE" => "MOBILE",
+						),
+					),
+					"STATUS_ID" => 8,
+					"ASSIGNED_BY_ID" => 8856,
+				),
+				'params' => array("REGISTER_SONET_EVENT" => "N")    // Y = произвести регистрацию события добавления лида в живой ленте. Дополнительно будет отправлено уведомление ответственному за лид.	
+			));
+
+			// отправляем запрос в Б24 и обрабатываем ответ
+			$curl = curl_init();
+			curl_setopt_array($curl, array(
+				CURLOPT_SSL_VERIFYPEER => 0,
+				CURLOPT_POST => 1,
+				CURLOPT_HEADER => 0,
+				CURLOPT_RETURNTRANSFER => 1,
+				CURLOPT_URL => $queryURL,
+				CURLOPT_POSTFIELDS => $queryData,
+			));
+			$result = curl_exec($curl);
+			curl_close($curl);
+			$result = json_decode($result, 1);
+
+			// если произошла какая-то ошибка - выведем её
+			if (array_key_exists('error', $result)) {
+				die("Ошибка при сохранении лида: " . $result['error_description']);
+			}
+			echo "Лид добавлен" . "\n";
+			$message->setFlag('Seen');
 		} else {
-			echo "no isset";
+			echo "Уже просмотренно";
 		}
-
-
-
-		//print_r($textMail);
-		//$message->setFlag('Seen');
 	}
-}
-
-
-function addB24(): string
-{
-	return "Лид добавлен";
 }
